730f463e66096819f9f532aa848c9508a054d206
lars-christian.simon <lars-christian.simon@ewe.de>
2018-09-19 14:33:29 +0200

    added lifecycle hooks to server.component.ts

--- src/app/assignments/databinding-deep-dive/server-element/server-element.component.ts
+++ src/app/assignments/databinding-deep-dive/server-element/server-element.component.ts
@@ -1,4 +1,16 @@
-import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit, AfterViewChecked, AfterViewInit,
+  Component,
+  DoCheck,
+  ElementRef,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+  ViewChild,
+  ViewEncapsulation
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -10,14 +22,49 @@ import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
   `],
   encapsulation: ViewEncapsulation.None // Emulated, Native, with None the style is applied application-wide
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements
+    OnChanges,
+    OnInit,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked {
 
   // @Input() element: {type: string, serverName: string, serverContent: string};
   @Input() serverName: string;
 
+  @ViewChild('heading') header: ElementRef;
+
   constructor() { }
 
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called!');
+  }
+
   ngOnInit() {
+    console.log('ngOnInit called!');
+    console.log('Text content: ' + this.header.nativeElement.textContent);
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck called!');
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit called!');
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked called!');
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit called!');
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked called!');
   }
 
 }
