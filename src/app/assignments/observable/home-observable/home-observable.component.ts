2eee95e250f0f5d05b3514273c5baff772d70858
lars-christian.simon <lars-christian.simon@ewe.de>
2018-10-26 14:43:45 +0200

    unsubscribe from observable to prevent memory leaks

--- src/app/assignments/observable/home-observable/home-observable.component.ts
+++ src/app/assignments/observable/home-observable/home-observable.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Observer, Subscription } from 'rxjs/Rx';
+import 'rxjs/Rx';
 
 @Component({
   selector: 'app-home-observable',
   templateUrl: './home-observable.component.html',
   styleUrls: ['./home-observable.component.css']
 })
-export class HomeObservableComponent implements OnInit {
+export class HomeObservableComponent implements OnInit, OnDestroy {
+
+  numberObservableSubscription: Subscription;
+  customObservableSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
+    const myNumbers = Observable.interval(1000);
+    this.numberObservableSubscription = myNumbers.subscribe(
+      (number: number) => {
+        console.log(number);
+      }
+      // , () => {}
+      // , () => {}
+    );
+   const myObservable = Observable.create((observer: Observer<string>) => {
+      setTimeout(() => {
+        // Push next data package
+        observer.next('first package');
+      }, 2000);
+      setTimeout(() => {
+        // Push next data package
+        observer.next('second package');
+      }, 4000);
+      setTimeout(() => {
+        // Push next data package
+        observer.complete();
+        // observer.error('this does not work');
+      }, 5000);
+      setTimeout(() => {
+        // Push next data package
+        observer.next('third package');
+      }, 6000);
+    });
+    this.customObservableSubscription = myObservable.subscribe(
+      (data: string) => {console.log(data)},
+      (error: string) => {console.log(error)},
+      () => {console.log('completed')}
+      );
+  }
+
+  ngOnDestroy() {
+    this.numberObservableSubscription.unsubscribe();
+    this.customObservableSubscription.unsubscribe();
   }
 
 }
