d650b0cf152dee8947bedc93aabbf7b8ddb0bdef
lars-christian.simon <lars-christian.simon@ewe.de>
2018-11-05 14:19:25 +0100

    completed assignment for forms template-driven approach

--- src/app/assignments/forms/forms-template-driven-assignment/forms-template-driven-assignment.component.ts
+++ src/app/assignments/forms/forms-template-driven-assignment/forms-template-driven-assignment.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-forms-template-driven-assignment',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormsTemplateDrivenAssignmentComponent implements OnInit {
 
+  @ViewChild('f') signupForm: NgForm;
+
+  defaultSubscription = 'advanced';
+  user = {
+    email: '',
+    subscription: '',
+    password: ''
+  }
+
+  submitted = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    console.log(this.signupForm);
+    this.user.email = this.signupForm.value.email;
+    this.user.subscription = this.signupForm.value.subscription;
+    this.user.password = this.signupForm.value.securityData.password;
+    console.log(this.user);
+
+    this.submitted = true;
+
+    this.signupForm.reset();
+    this.signupForm.form.patchValue({
+      email: '',
+      subscription: 'advanced',
+      password: ''
+    });
   }
 
 }
