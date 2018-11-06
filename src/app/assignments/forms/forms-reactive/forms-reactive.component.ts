4a86633b747911cd4e8cf4f130278a7b2c0b2d2d
lars-christian.simon <lars-christian.simon@ewe.de>
2018-11-06 14:37:43 +0100

    writing custom validator

--- src/app/assignments/forms/forms-reactive/forms-reactive.component.ts
+++ src/app/assignments/forms/forms-reactive/forms-reactive.component.ts
@@ -14,15 +14,15 @@ import {
 export class FormsReactiveComponent implements OnInit {
 
   genders = ['male', 'female'];
-
   signupForm: FormGroup;
+  forbiddenUsernames = ['Chris', 'Anna'];  
 
   constructor() { }
 
   ngOnInit() {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
         'email': new FormControl(null, [Validators.required, Validators.email]),
       }),
       'gender': new FormControl('male'),
@@ -38,6 +38,14 @@ export class FormsReactiveComponent implements OnInit {
     // Without casting the hobbies FormArray in the Form to the type FormArray there would be an error
     const control = new FormControl(null, Validators.required);
     (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+  // Specifying the return in typescript as a JSON object by describing the key-value-pair
+  forbiddenNames(control: FormControl): {[s: string]: boolean} {
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return {'nameIsForbidden': true};
+    }
+    return null;
   }
 
 }
